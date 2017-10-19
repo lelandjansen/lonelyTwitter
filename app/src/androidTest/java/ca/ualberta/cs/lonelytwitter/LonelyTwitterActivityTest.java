4c78aa98bfbd5eb37b1dcec28e4f5bbb7f529826
Leland Jansen <lelandjansen@users.noreply.github.com>
2017-10-19 13:24:43 -0600

    Work on Lab 7 (does not build)

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,17 +2,47 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+public class LonelyTwitterActivityTest
+        extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void tearDown() {
+        solo.finishOpenedActivities();
     }
 
     public void testStart() throws Exception {
         Activity activity = getActivity();
 
     }
+
+    public void testTweet() {
+        solo.assertCurrentActivity("some activity",
+                LonelyTwitterActivity.class);
+        String text = "http://bit.ly/IqT6zt";
+        EditText editText = (EditText) solo.getView(R.id.body);
+        solo.enterText(editText, text);
+        solo.clickOnButton("Save");
+//        solo.enterText(editText, "");
+        assertTrue(solo.waitForText(text));
+    }
+
+    public void testClickTweetList() {
+
+    }
+
+
 }
